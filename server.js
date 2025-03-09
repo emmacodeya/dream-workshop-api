3c6aa4439ec815fefce38a6f3522ad842f91e7b0
emmacodeya <kkes60128angel@gmail.com>
2025-03-09 18:38:46 +0800

    Add PATCH support for JSON Server persistence

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
+const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); 
-const middlewares = jsonServer.defaults({ static: "./public" }); 
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
 server.use(cors());
-server.use("/assets", jsonServer.defaults({ static: path.join(__dirname, "public") }));
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/assets", jsonServer.static(path.join(__dirname, "public")));
+server.use((req, res, next) => {
+  if (["POST", "PUT", "DELETE", "PATCH"].includes(req.method)) {
+    setTimeout(() => {
+      fs.writeFileSync(path.join(__dirname, "db.json"), JSON.stringify(router.db.getState(), null, 2));
+    }, 500); 
+  }
+  next();
+});
+
+
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
